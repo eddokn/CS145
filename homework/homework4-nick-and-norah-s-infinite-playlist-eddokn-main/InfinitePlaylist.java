ea46065dbe4bc72c14a7ec78a18c07313d387c22
eddokn <graysonkoch3@gmail.com>
2023-05-03 16:34:58 -0700

    homework 4 work

--- homework/homework4-nick-and-norah-s-infinite-playlist-eddokn-main/InfinitePlaylist.java
+++ homework/homework4-nick-and-norah-s-infinite-playlist-eddokn-main/InfinitePlaylist.java
@@ -1,13 +1,13 @@
-public class InfinitePlaylist {
+     public class InfinitePlaylist {
+  //attributes
     private String title;
     private int size;
     public SongNode currentNode;
 
     public InfinitePlaylist(String title) {
       this.title = title;
-      this.head = null;
-      this.tail = null;
       this.size = 0;
+      this.currentNode = null;
     }
 
     public String getTitle() {
@@ -19,23 +19,32 @@ public class InfinitePlaylist {
     }
 
     public Song play() {
-      currentNode = currentNode.setNext();
-      return currentNode.setPrevious();
+      Song song = this.currentNode.song;
+      this.currentNode = this.currentNode.nextNode;
+      return song;
     }
 
     public void add(Song song) {
-      currentNode.setNext(song);
+      SongNode newSong = new SongNode(song, this.currentNode.nextNode, this.currentNode.previousNode);
+      if(this.currentNode.song==null){
+        this.currentNode.song = song;
+        this.currentNode.nextNode = null;
+        this.currentNode.nextNode = null;
+      } else{
+        this.currentNode.previousNode = this.currentNode;
+        this.currentNode = this.currentNode.nextNode;
+        this.currentNode.song = song;
+      }
       this.size +=1;
     }
 
     public void reset(SongIterator it) {
       // necessary step: cast songiterator as a songiteratorimpl
       SongIteratorImpl itimpl = (SongIteratorImpl) it;
-      return itimpl;
     }
 
     public SongIterator iterator() {
-      SongIterator it = currentNode;
+      SongIterator it = (SongIterator) currentNode;
       return it;
     }
 
